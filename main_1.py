103e08b576284e01a4ebc5c40199d5d351c367c5
Alexkorkod <alexkorkod@gmail.com>
2016-09-30 19:31:55 +0300

    real field action

--- main_1.py
+++ main_1.py
@@ -131,7 +131,7 @@ def findLeastExpensiveCardCost(hand):
     return min_cost
 
 def makeTurn(deck,hand,mana,replace_count):
-    global sum_lost_mana, cur_mana, field, enemy_gen, game_stats_per_mana,played_cards
+    global sum_lost_mana, cur_mana, field, enemy_gen, game_stats_per_mana,played_cards,side
     cards_to_play = choseCardForTurn(hand,mana)
     mana_left = mana
     if len(cards_to_play) > 0 :
@@ -143,18 +143,15 @@ def makeTurn(deck,hand,mana,replace_count):
                         if card_from_hand['mana_cost'] == '0':
                             card_from_hand['mana_cost'] = '1'
                         game_stats_per_mana += (int(card_from_hand['attack'])+int(card_from_hand['health']))/float(card_from_hand['mana_cost'])
-                    played_cards += 1
-                    adj = getAdjForPlacement()
-                    index = random.randint(0,len(adj)-1)
-                    chosen_place = adj[index]
-                    for tile in field:
-                        if tile == chosen_place:
-                            tile['card'] = card_from_hand
-                            break
-            if played_card['attack'] != '':
-                enemy_gen['hp'] -= int(played_card['attack'])
-            else:
-                enemy_gen['hp'] -= 1
+                        played_cards += 1
+                        adj = getAdjForPlacement(side)
+                        index = random.randint(0,len(adj)-1)
+                        chosen_place = adj[index]
+                        for tile in field:
+                            if tile == chosen_place:
+                                card_from_hand['side'] = side
+                                tile['card'] = card_from_hand
+                                break
             mana_left = mana - int(played_card['mana_cost'])
     elif replace_count < 1:
         hand = replace(deck,hand)
@@ -192,13 +189,23 @@ def initialPlacement():
         if tile['pos'] == fp_gen_pos:
             tile['card'] = general
         if tile['pos'] == sp_gen_pos:
-            tile['card'] = {'attack':2,'health':25,'type':'GENERAL','side':'second'}
+            tile['card'] = enemy_general
 
-def getAdjForPlacement():
+def getAdjForMove(card):
     global field
     adj = []
     for tile in field:
-        if tile['card'] != {} and tile['card']['side'] == 'first':
+        if tile['card'] == card:
+            cur_adj = getAdj(tile)
+            for place in cur_adj:
+                adj.append(place)
+    return adj
+
+def getAdjForPlacement(side):
+    global field
+    adj = []
+    for tile in field:
+        if tile['card'] != {} and tile['card']['side'] == side:
             cur_adj = getAdj(tile)
             for place in cur_adj:
                 adj.append(place)  
@@ -216,29 +223,98 @@ def getAdj(cur_tile):
         if tile['pos']['i'] in range_i and tile['pos']['j'] in range_j:
             adj.append(tile)
     tile = []
-    for (i,tile) in enumerate(adj):
-        if tile['card'] != {}:
-            adj.pop(i)
+    adj = dict((i,el) for i,el in enumerate(adj))
+    tiles_with_cards = []
+    for i,tile in adj.items():
+        if tile['card']:
+            tiles_with_cards.append(i)
+    for i in tiles_with_cards:
+        adj.pop(i)
+    adj = adj.values()
     return adj
+
+def firstIterationTrade():
+    #TODO trade with nearest THEN TODO trade with adj
+    global field, side, other_side
+    for tile in field:
+        if tile['card'] != {} and tile['card']['side'] == side:
+            for deep_tile in field:
+                if deep_tile['card'] != {} and deep_tile['card']['side'] == other_side:
+                    tile['card']['health'] = int(tile['card']['health']) - int(deep_tile['card']['attack'])
+                    deep_tile['card']['health'] = int(deep_tile['card']['health']) - int(tile['card']['attack'])
+                    if int(tile['card']['health']) <= 0:
+                        tile['card'] = {}
+                    if int(deep_tile['card']['health']) <= 0:
+                        deep_tile['card'] = {}
+                    break
+            break
+
+def showField():
+    #TODO make this shit beatyfull
+    global field
+    local_field = {'row1':{},'row2':{},'row3':{},'row4':{},'row5':{}}
+    for tile in field:
+        if tile['pos']['j'] == 1:
+            if tile['card'] != {}:
+                local_field['row1'][tile['pos']['i']] = '|%2s:%2s|' % (tile['card']['attack'],tile['card']['health'])
+            else:
+                local_field['row1'][tile['pos']['i']] = '|  :  |'
+        elif tile['pos']['j'] == 2:
+            if tile['card'] != {}:
+                local_field['row2'][tile['pos']['i']] = '|%2s:%2s|' % (tile['card']['attack'],tile['card']['health'])
+            else:
+                local_field['row2'][tile['pos']['i']] = '|  :  |'
+        elif tile['pos']['j'] == 3:
+            if tile['card'] != {}:
+                local_field['row3'][tile['pos']['i']] = '|%2s:%2s|' % (tile['card']['attack'],tile['card']['health'])
+            else:
+                local_field['row3'][tile['pos']['i']] = '|  :  |'
+        elif tile['pos']['j'] == 4:
+            if tile['card'] != {}:
+                local_field['row4'][tile['pos']['i']] = '|%2s:%2s|' % (tile['card']['attack'],tile['card']['health'])         
+            else:
+                local_field['row4'][tile['pos']['i']] = '|  :  |'
+        elif tile['pos']['j'] == 5:
+            if tile['card'] != {}:
+                local_field['row5'][tile['pos']['i']] = '|%2s:%2s|' % (tile['card']['attack'],tile['card']['health'])
+            else:
+                local_field['row5'][tile['pos']['i']] = '|  :  |'
+    for key, row in local_field.items():
+        l = row.keys()
+        l = list(l)
+        l.sort()
+        for i in l:
+            sys.stdout.write(row[i])
+        sys.stdout.write('\n')
+    sys.stdout.write('---------------------------------\n')
 
 def placeCard(card):
     global field
     
-def moveGeneral():
-    global general
-    adj = getAdjForPlacement()
-    index = random.randint(0,len(adj)-1)
-    chosen_place = adj[index]
-    for tile in field:
-        if tile == chosen_place:
-            tile['card'] = general
-        elif tile['card'] != {} and tile['card']['type'] == 'GENERAL' and tile['card']['side'] == 'first':
-            tile['card'] = {}
+def moveGeneral(side):
+    if side == 'first':
+        cur_gen = general
+    else:
+        cur_gen = enemy_general
+    adj = getAdjForMove(cur_gen)
+    if len(adj) > 0:
+        index = random.randint(0,len(adj)-1)
+        chosen_place = adj[index]
+        for tile in field:
+            if tile == chosen_place:
+                tile['card'] = cur_gen
+            elif tile['card'] != {} and tile['card']['type'] == 'GENERAL' and tile['card']['side'] == side:
+                tile['card'] = {}   
 
 c_limit = 1000
 cc_limit = 1000
+show_field = False
 if len(sys.argv) > 1:
     c_limit = int(sys.argv[1])
+if len(sys.argv) > 2:
+    cc_limit = int(sys.argv[2])
+if len(sys.argv) > 3:
+    show_field = True
 backup_ar = list(ar)
 deck_info = []
 c = 0
@@ -263,7 +339,7 @@ while c < c_limit:
             limitk = random.randint(2,3)
             while k < limitk:
                 card['side'] = 'first'
-                deck.append(card)
+                deck.append(card.copy())
                 i += 1
                 k += 1
                 if i == 39:
@@ -272,47 +348,62 @@ while c < c_limit:
     backup_deck = list(deck)
 
     cc = 0
-    hand = []
-    field = []
     sum_lost_mana = 0
     sum_hand_size = 0
     sum_stats_per_mana = 0
+    hand = []
+    field = []
     turns = 0
     while cc < cc_limit:
-    	enemy_gen = {'hp':25}
-        deck = list(backup_deck)
+        side = 'first'
+        other_side = 'second'
+    	enemy_general = {'attack':2,'health':25,'type':'GENERAL','side':other_side}
+        for card in backup_deck:
+            deck.append(card.copy())
+        if deck == backup_deck:
+            for card in deck:
+                sys.stdout.write(str(card['label'])+':'+str(card['health']))
+                sys.stdout.write('\n')
+            sys.stdout.write('-----------------------\n')
         simulateFirstDraw(deck,hand)
         mullForFirstTurn(deck,hand,2,0)
-        general = {'attack':2,'health':25,'type':'GENERAL','side':'first'}
+        general = {'attack':2,'health':25,'type':'GENERAL','side':side}
         generateField()
         initialPlacement()
         played_cards = 0
         game_stats_per_mana = 0
         mana = 2
-        while enemy_gen['hp'] > 0: 
+        while enemy_general['health'] > 0: 
             turns += 1
             cur_mana = mana
-            moveGeneral()
+            moveGeneral(side)
             makeTurn(deck,hand,mana,0)
+            moveGeneral(other_side)
+            firstIterationTrade()
             endTurn(deck,hand)
             if mana < 9:
             	mana += 1
-        sum_stats_per_mana += game_stats_per_mana/float(played_cards)
+            if show_field:
+                showField()
+        if played_cards > 0:
+            sum_stats_per_mana += game_stats_per_mana/float(played_cards)
         cc += 1
         hand = []
         field = []
     c += 1
-    sys.stdout.write('%(progress)2.2f%% done\r' % {'progress': (c*c_limit)/float(c_limit*c_limit/100)})
-    sys.stdout.flush()
-    if sum_lost_mana < lost_mana and sum_hand_size > hand_size and sum_stats_per_mana > stats_per_mana:
-        stats_per_mana = sum_stats_per_mana
-        lost_mana = sum_lost_mana
-        hand_size = sum_hand_size
-        best_deck = list(backup_deck)
-        made_turns = turns/float(cc_limit)
-best_deck.append({'stats_per_mana':stats_per_mana/1000.0})
-best_deck.append({'turns':made_turns})
-best_deck.append({'avg_hand_size':hand_size/(int(made_turns)*1000.0)})
-best_deck.append({'lost_mana':lost_mana/1000.0})
-deck_info.append(best_deck)
-json.dump(deck_info,open('best_deck.json','w'),indent=4)
+    if len(sys.argv) <= 2:
+        sys.stdout.write('%(progress)2.2f%% done\r' % {'progress': (c*c_limit)/float(c_limit*c_limit/100)})
+        sys.stdout.flush()
+        if sum_lost_mana < lost_mana and sum_hand_size > hand_size and sum_stats_per_mana > stats_per_mana:
+            stats_per_mana = sum_stats_per_mana
+            lost_mana = sum_lost_mana
+            hand_size = sum_hand_size
+            best_deck = list(backup_deck)
+            made_turns = turns/float(cc_limit)
+if len(sys.argv) <= 2:
+    best_deck.append({'stats_per_mana':stats_per_mana/cc_limit})
+    best_deck.append({'turns':made_turns})
+    best_deck.append({'avg_hand_size':hand_size/(made_turns*cc_limit)})
+    best_deck.append({'lost_mana':lost_mana/cc_limit})
+    deck_info.append(best_deck)
+    json.dump(deck_info,open('best_deck.json','w'),indent=4)
